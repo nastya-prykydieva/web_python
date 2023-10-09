05e7e2b99beb70e71ba6ca2fdadc1563739315f9
nastya-prykydieva <134232781+nastya-prykydieva@users.noreply.github.com>
2023-10-09 06:17:56 +0300

    Add task 1.3 (loop for a chat)

--- lab1/task1/client.py
+++ lab1/task1/client.py
@@ -6,9 +6,15 @@ port = 55555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-message = input("Enter text to send to the server: ")
-s.send(message.encode('utf-8'))
+while True:
+    message = input("Enter text to send to the server: ")
+    s.send(message.encode('utf-8'))
 
-response = s.recv(1024).decode('utf-8')
-print(f"Response from the server: \n{response}")
+    if message == "stop":
+        s.close()
+        break
+
+    response = s.recv(1024).decode('utf-8')
+    print(f"Response from the server: \n{response}")
+
 s.close()
--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -6,28 +6,33 @@ port = 55555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
-s.listen(1)
+s.listen(5)
 print(f"Server is listening on {host}, {port}")
 
 conn, addr = s.accept()
 print(f'Connected with {str(addr)}')
 
-data = conn.recv(1024).decode('utf-8')
-current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-print(f"\nReceived from the client: {data}")
-print(f"time: {current_time}")
+while True:
+    data = conn.recv(1024).decode('utf-8')
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"\nReceived from the client: {data}")
+    print(f"time: {current_time}")
 
-time.sleep(5)
+    if data == "stop":
+        conn.close()
+        print("\nConnection was closed by client.")
+        break
+    else:
+        time.sleep(5)
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        response = conn.send(data.encode('utf-8'))
+        print(f"Replied to the client.")
+        print(f"time: {current_time}")
 
-current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-response = conn.send(data.encode('utf-8'))
-print(f"\nReplied to the client.")
-print(f"time: {current_time}")
-
-if len(data) == response:
-    print("\nThe text was sent successfully!")
-else:
-    print("\nSomething went wrong!")
+        if len(data) == response:
+            print("The text was sent successfully!")
+        else:
+            print("\nSomething went wrong!")
 
 conn.close()
 s.close()
