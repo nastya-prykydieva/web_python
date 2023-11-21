45132c58cf296b9f65eab0d9922cae0c10443283
nastya-prykydieva <134232781+nastya-prykydieva@users.noreply.github.com>
2023-11-21 06:05:49 +0200

    Lab 9 (add tasks)

---
+++ lab9/app/models.py
@@ -0,0 +1,47 @@
+from app import db, login_manager
+from flask_login import UserMixin
+from flask_bcrypt import check_password_hash, generate_password_hash
+from datetime import datetime
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(255))
+    complete = db.Column(db.Boolean)
+
+
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    text = db.Column(db.String(500))
+    date = db.Column(db.DateTime)
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password_hash = db.Column(db.String(128), nullable=False)
+    about_me = db.Column(db.String(500))
+    last_seen = db.Column(db.DateTime, default=datetime.now())
+
+    @property
+    def password(self):
+        return AttributeError('Password is not a readable attribute.')
+
+    @password.setter
+    def password(self, psw):
+        self.password_hash = generate_password_hash(psw).decode('utf-8')
+
+    def verify_password(self, psw):
+        return check_password_hash(self.password_hash, psw)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
---
+++ lab9/app/views.py
@@ -0,0 +1,303 @@
+from flask import request, render_template, redirect, url_for, make_response, session, flash
+from app import app, db
+from .models import Todo, Feedback, User
+from datetime import datetime
+from .forms import RegistrationForm, LoginForm, ChangePasswordForm, ToDoForm, FeedbackForm, UpdateAccountForm
+from flask_login import login_user, current_user, logout_user, login_required
+from PIL import Image
+import json
+import os
+import secrets
+
+
+@app.context_processor
+def inject_system_info():
+    return dict(data=os.name, user_agent=request.headers.get('User-Agent'), time=datetime.now())
+
+
+@app.route('/')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/ipz')
+def ipz():
+    return render_template('ipz.html')
+
+
+skills = ["Python", "SQL", "Flask", "English", "Teamwork"]
+
+
+@app.route('/skills')
+@app.route('/skills/<int:num>')
+def skills_page(num=None):
+    if num is not None and num < len(skills):
+        return render_template('skill.html', num=num+1, skill=skills[num])
+    else:
+        return render_template('skills.html', skills=skills)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+def add_file_data():
+    try:
+        with open('app/file.json', 'r') as file:
+            d = json.load(file)
+        return d
+    except FileNotFoundError:
+        return flash("File doesn't exist!")
+
+
+@app.route('/info')
+@login_required
+def info():
+    form = ChangePasswordForm()
+    username = current_user.username
+    return render_template('info.html', form=form, username=username, cookies=request.cookies)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        flash('You are logged in.', category='success')
+        return redirect(url_for('info'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        flash('You are logged in.', category='success')
+        return redirect(url_for('info'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.verify_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('You have been logged in!', category='success')
+            return redirect(url_for('info'))
+        else:
+            flash('Error! Please try again.', category='danger')
+            return redirect(url_for('login'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    flash('You have been logged out.', category='success')
+    logout_user()
+    return redirect(url_for('login'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+@app.after_request
+def after_request(response):
+    if current_user:
+        current_user.last_seen = datetime.now()
+        try:
+            db.session.commit()
+        except:
+            flash('Error while update user last seen!', 'danger')
+    return response
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your account has been updated!', category='success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.about_me.data = current_user.about_me
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+@login_required
+def reset_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.new_password.data
+            db.session.commit()
+            flash("Your password has been changed.", "success")
+            return redirect(url_for('account'))
+        flash("Incorrect old password.", "danger")
+        return redirect(url_for('reset_password'))
+    return render_template('password.html', form=form)
+
+
+@app.route('/users')
+def users():
+    all_users = User.query.all()
+    return render_template('users.html', all_users=all_users)
+
+
+@app.route('/add_cookie', methods=['POST'])
+def add_cookie():
+    form = ChangePasswordForm()
+    cookies = request.cookies
+    key = request.form.get('key')
+    value = request.form.get('value')
+    max_age = int(request.form.get('max_age'))
+    resp = make_response(render_template('info.html', username=current_user.username, cookies=cookies, form=form))
+    resp.set_cookie(key, value, max_age=max_age)
+    flash("Cookie is added! Please, refresh the page.", "success")
+    return resp
+
+
+@app.route('/remove_cookie', methods=['POST'])
+def remove_cookie():
+    form = ChangePasswordForm()
+    cookies = request.cookies
+    key = request.form.get('key')
+    resp = make_response(render_template('info.html', username=current_user.username, cookies=cookies, form=form))
+    if key in cookies:
+        resp.delete_cookie(key)
+        flash("Cookie is deleted! Please, refresh the page.", "success")
+    return resp
+
+
+@app.route('/remove_all_cookies', methods=['POST'])
+def remove_all_cookies():
+    form = ChangePasswordForm()
+    cookies = request.cookies
+    resp = make_response(render_template('info.html', username=current_user.username, cookies=cookies, form=form))
+    for key in cookies.keys():
+        print(key)
+        if key != 'session':
+            resp.delete_cookie(key)
+    flash("All cookies are deleted!", "success")
+    return resp
+
+
+@app.route('/change_password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        old_password = form.old_password.data
+        new_password = form.new_password.data
+        confirm_password = form.confirm_password.data
+        file_data = add_file_data()
+        try:
+            json_password = file_data['password']
+        except TypeError:
+            flash("Error with data!", "error")
+            raise SystemExit("Error with data!")
+        if old_password == json_password:
+            if new_password == confirm_password:
+                session['password'] = new_password
+                with open('app/file.json', "w") as file:
+                    file_data['password'] = new_password
+                    json.dump(file_data, file)
+                    flash("Password was changed.", "success")
+                    return redirect(url_for('info'))
+        flash("Password was not changed.", "danger")
+        return redirect(url_for('info'))
+    return render_template('info.html', form=form)
+
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    form = ToDoForm()
+    todo_list = db.session.query(Todo).all()
+    if form.validate_on_submit():
+        return redirect(url_for("todo"))
+    return render_template("todo.html", form=form, todo_list=todo_list)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    form = ToDoForm()
+    todo_list = Todo.query.all()
+    if form.validate_on_submit():
+        new_task = Todo(title=form.todo.data, description=form.description.data, complete=False)
+        db.session.add(new_task)
+        db.session.commit()
+        flash("The new task was added successfully.", "success")
+        return redirect(url_for("todo"))
+    flash("The new task was not added!", "danger")
+    return render_template('todo.html', form=form, todo_list=todo_list)
+
+
+@app.route("/update/<int:id>")
+def update(id):
+    todo = Todo.query.get_or_404(id)
+    todo.complete = not todo.complete
+    db.session.commit()
+    flash(f"The task №{id} was updated.", "success")
+    return redirect(url_for("todo"))
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    todo = Todo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
+    flash(f"Task №{id} was deleted.", "success")
+    return redirect(url_for("todo"))
+
+
+@app.route("/feedback", methods=['GET', 'POST'])
+def feedback():
+    form = FeedbackForm()
+    feedbacks = db.session.query(Feedback).all()
+    if form.validate_on_submit():
+        username = form.username.data
+        text = form.text.data
+        date = datetime.now()
+        if username and text:
+            db.session.add(Feedback(username=username, text=text, date=date))
+            db.session.commit()
+            flash("Feedback was added.", category="success")
+        else:
+            flash("Feedback was not added.", category="danger")
+        return redirect(url_for("feedback"))
+    return render_template('feedback.html', feedbacks=feedbacks, form=form)
+
+
+@app.route("/feedbacks/delete/<int:id>")
+def delete_feedback(id):
+    feedback = Feedback.query.get_or_404(id)
+    try:
+        db.session.delete(feedback)
+        db.session.commit()
+        flash("Feedback was successfully deleted!", category="success")
+    except:
+        db.session.rollback()
+        flash("Error!", category="danger")
+    return redirect(url_for("feedback"))
